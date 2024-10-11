0e4bc1404e01371ee82d9723ac8aa2adf8cb69d3
Rohit Kumar <da24s003@smail.iitm.ac.in>
2024-10-11 12:12:39 +0530

    day 2

--- applications/model.py
+++ applications/model.py
@@ -1,8 +1,63 @@
 from applications.database import db
 
+# cust = Customer(address= 'asdf',
+#          phason ='saf')
+# user = User(username='admin',
+#             email= 'saf',
+#             role = 'cust',
+#             customer = [cust])
+
+# db.session.add(user)
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=True, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    approved = db.Column(db.Boolean(), default=False)
+
+    roles = db.relationship('Role', secondary='user_role', backref=db.backref('users', lazy=True))
+    customer_dets = db.relationship('Customer', backref='user', lazy=True, uselist=False)
+
+
+class Customer(db.Model):
+    table_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    address = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(10), nullable=False)
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+
+
+#  user_role
+class UserRole(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
+
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    selling_price = db.Column(db.Float, nullable=False)
+    cost_price = db.Column(db.Float, nullable=False)
+    mfg_date = db.Column(db.Date, nullable=False)
+    exp_date = db.Column(db.Date, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
